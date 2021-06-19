2548d4532d8c5b1ef1ae9cb35fc1de8bbee2afef
enimaloc <github@enimaloc.ga>
2021-06-19 03:51:54 +0200

    feat(logger): Add logger and reporting with Sentry
    Adding logger for readability and error reporting to the sentry
    dashboard.
    
    You can specify the logger level with the `logLevel`
    environnement variable, possibles levels are :
    - TRACE
    - DEBUG
    - INFO
    - WARN
    - ERROR
    
    You can define sentry DNS with `sentryDns` environnement
    variable.
    You can define sentry environnement to `Development` just by
    creating `dev` environnement variable and assigned any value.

--- src/main/java/com/github/enimaloc/yui/Main.java
+++ src/main/java/com/github/enimaloc/yui/Main.java
@@ -1,9 +1,34 @@
 package com.github.enimaloc.yui;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import io.sentry.Sentry;
+import org.slf4j.LoggerFactory;
+
+import java.util.Locale;
+
 public class Main {
     
     public static void main(String[] args) {
-    
+        ((Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME))
+                .setLevel(Level.toLevel(
+                        System.getenv("logLevel") != null ? System.getenv("logLevel").toUpperCase(Locale.ROOT) : null,
+                        Level.INFO
+                ));
+        
+        if (System.getenv("sentryDns") != null) {
+            Sentry.init(options -> {
+                options.setDsn(System.getenv("sentryDns"));
+                options.setDist(System.getProperty("os.arch"));
+                options.setEnvironment(System.getenv("dev") != null ? "Development" : "Production");
+            });
+        }
+        
+        try {
+            throw new Exception("Test Sentry report");
+        } catch (Exception e) {
+            LoggerFactory.getLogger(Main.class).error("That's suck", e);
+        }
     }
     
 }
