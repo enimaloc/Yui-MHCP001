7717817231c95b96d4b2bd2d0281e6c7e9b34907
enimaloc <antoine@enimaloc.fr>
2021-09-29 22:12:45 +0200

    feat(music): add lavaplayer requirement

--- src/main/java/fr/enimaloc/yui/Yui.java
+++ src/main/java/fr/enimaloc/yui/Yui.java
@@ -4,6 +4,7 @@ import com.jagrosh.jdautilities.command.CommandClient;
 import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import com.jagrosh.jdautilities.command.SlashCommand;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import fr.enimaloc.yui.music.MusicManager;
 import java.util.Arrays;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -18,22 +19,24 @@ public class Yui {
     private final EventWaiter   eventWaiter;
     private final CommandClient client;
     private final JDA           jda;
-    private final Logger        logger = LoggerFactory.getLogger(Yui.class);
+    private final Logger       logger = LoggerFactory.getLogger(Yui.class);
+    private final MusicManager musicManager;
     
     public Yui() throws LoginException {
         logger.info("Starting Yui-MHCP001 (v."+Constant.VERSION+")");
         Runtime.getRuntime().addShutdownHook(new Thread(this::disconnect));
         
         this.eventWaiter = new EventWaiter();
+        this.musicManager = new MusicManager();
 
         this.client = new CommandClientBuilder()
                 .setActivity(Activity.watching("players"))
                 .setHelpWord("help")
                 .setEmojis(Constant.EMOJI_SUCCESS, Constant.EMOJI_WARNING, Constant.EMOJI_ERROR)
-                .setPrefix(Constant.PREFIX)
                 .addSlashCommands(new SlashCommand[]{
                 })
                 .setOwnerId(Constant.OWNERS_ID[0]+"")
+                .forceGuildOnly(System.getenv("dev") != null ? "854508847896723526" : null)
                 .setCoOwnerIds(
                         Arrays.stream(Arrays.copyOfRange(Constant.OWNERS_ID, 1, Constant.OWNERS_ID.length))
                               .mapToObj(l -> l+"")
---
+++ src/main/java/fr/enimaloc/yui/music/MusicManager.java
@@ -0,0 +1,48 @@
+package fr.enimaloc.yui.music;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import java.util.HashMap;
+import java.util.Map;
+import net.dv8tion.jda.api.entities.Guild;
+
+public class MusicManager {
+
+    private final AudioPlayerManager playerManager;
+    private final Map<Long, GuildMusicManager> musicManagers;
+
+    public MusicManager() {
+        this.playerManager = new DefaultAudioPlayerManager();
+        AudioSourceManagers.registerRemoteSources(playerManager);
+        AudioSourceManagers.registerLocalSource(playerManager);
+
+        this.musicManagers = new HashMap<>();
+    }
+
+    public synchronized GuildMusicManager getGuildAudioPlayer(Guild guild) {
+        GuildMusicManager musicManager = musicManagers.get(guild.getIdLong());
+        if (musicManager == null) {
+            musicManager = new GuildMusicManager(playerManager);
+            musicManagers.put(guild.getIdLong(), musicManager);
+        }
+        guild.getAudioManager().setSendingHandler(musicManager.getSendHandler());
+        return musicManager;
+    }
+
+    public void load(Guild guild, String trackUrl, AudioLoadResultHandler handler) {
+        GuildMusicManager musicManager = getGuildAudioPlayer(guild);
+
+        playerManager.loadItemOrdered(musicManager, trackUrl, handler);
+    }
+
+    public void play(Guild guild, AudioTrack track) {
+        getGuildAudioPlayer(guild).scheduler.queue(track);
+    }
+
+    public void skip(Guild guild) {
+        getGuildAudioPlayer(guild).scheduler.nextTrack();
+    }
+}
