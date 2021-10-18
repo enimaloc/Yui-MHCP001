715d417b36361bc5a7efd8dd92633a45017b40a6
Kévin <kevin.alves72100@gmail.com>
2021-10-18 17:00:11 +0200

    feat(moderation): add moderation command class

---
+++ src/main/java/fr/enimaloc/yui/commands/ModerationCommand.java
@@ -0,0 +1,11 @@
+package fr.enimaloc.yui.commands;
+
+import com.jagrosh.jdautilities.command.SlashCommand;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+
+public class ModerationCommand extends SlashCommand {
+    @Override
+    protected void execute(SlashCommandEvent event) {
+
+    }
+}
